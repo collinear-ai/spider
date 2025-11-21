8c37ec21a40a17e712cfadb96599ea3b126f9b92
Muyu He <muyuhe0327@gmail.com>
2025-11-21 15:20:04 -0800

    Set logging level

--- server/app.py
+++ server/app.py
@@ -16,15 +16,10 @@ from .executor import JobExecutionError, run_generation_job, _job_snapshot
 from . import events
 
 def _configure_executor_logging() -> None:
-    logger = logging.getLogger("server.executor")
-    logger.setLevel(logging.INFO)
-    if logger.handlers:
-        return
-    handler = logging.StreamHandler()
-    handler.setLevel(logging.INFO)
-    formatter = logging.Formatter("%(asctime)s [%(levelname)s] %(message)s")
-    handler.setFormatter(formatter)
-    logger.addHandler(handler)
+    logging.basicConfig(
+        level=logging.INFO,
+        format="%(asctime)s [%(levelname)s] %(name)s: %(message)s",
+    )
 
 _configure_executor_logging()
 
